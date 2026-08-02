94f1565913cb64097e66b02c542f84e3d1b1332a
amanpy45-png <263108657+amanpy45-png@users.noreply.github.com>
2026-08-02 22:05:39 +0530

    Add files via upload

---
+++ week7/utils/vectorstore.py
@@ -0,0 +1,13 @@
+from langchain_community.vectorstores import FAISS
+
+
+def create_vectorstore(chunks, embeddings):
+
+    vectorstore = FAISS.from_documents(
+        chunks,
+        embeddings
+    )
+
+    print("Vector Store Created Successfully!")
+
+    return vectorstore
