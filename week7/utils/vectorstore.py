548caa55f0bec77ca28e80b1d3b7b2d95a9a1c63
amanpy45-png <263108657+amanpy45-png@users.noreply.github.com>
2026-08-02 22:17:08 +0530

    Update vectorstore.py

--- week7/utils/vectorstore.py
+++ week7/utils/vectorstore.py
@@ -1,13 +1,9 @@
 from langchain_community.vectorstores import FAISS
 
-
 def create_vectorstore(chunks, embeddings):
-
     vectorstore = FAISS.from_documents(
         chunks,
         embeddings
     )
-
     print("Vector Store Created Successfully!")
-
     return vectorstore
